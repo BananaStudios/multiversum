08db122c1b1e6c916337fd4fc4803dd21e5850db
Harisan02 <531493@edu.rocmn.nl>
2020-03-10 10:18:35 +0100

    sfs

--- view/navbar.php
+++ view/navbar.php
@@ -1,24 +1,42 @@
 <html>
-<link rel="stylesheet" type="text/css" href="./style/style.css">
-
-<nav class="navbar" role="navigation">
-                <ul class="nav navbar-nav navbar-right">
-<link rel="stylesheet" type="text/css" href="style/style.css">
-
-<nav class="navbar">
-                <ul class="navbar">
-                    <li>
-                        <a href="#home">Home</a>
-                    </li>
-                    <li>
-                        <a href="#about">About</a>
-                    </li>
-                    <li>
-                        <a href="#portfolio">Portfolio</a>
-                    </li>
-                   <li>
-                        <a href="contact.php">Contact</a>
-                    </li>
-                </ul>
+    <link rel="stylesheet" type="text/css" href="./style/navbar.css">
+<div class="container">
+    <div class="navbar-header">
+      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href="./" class="navbar-brand">Bootstrap Menu</a>
+    </div>
+    <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+      <ul class="nav navbar-nav navbar-right">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">About <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Mission</a></li>
+            <li><a href="#">Vision</a></li>
+            <li><a href="#">Careers</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Products</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li class="active">
+          <a href="#">Contact</a>
+        </li>
+      </ul>
     </nav>
+  </div>
+
+ <script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js'></script>
+</body>
 </html>
