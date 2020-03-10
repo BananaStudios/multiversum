669c48c4e972c6457541de9242113132016ab099
iman-nab <iman_nab@hotmail.com>
2020-03-10 09:28:47 +0100

    d

--- view/index.php
+++ view/index.php
@@ -1,37 +1,8 @@
 <?php
 
-        <?php 
-include("./header.php");
-include("../dbase/config.php");
-include("../dbase/dbopen.php");
 require("header.php");
-
 require("content.php");
 
+require("footer.php");
 
-if($preparedquery->errno) {
-  echo "Fout bij het uitvoeren van commando";
-} else {
-  $result = $preparedquery->get_result();
-  
-  if($result->num_rows === 0) {
-    echo "Geen rijen gevonden";
-  } else {
-    
-    while($row = $result->fetch_assoc()) {
-      echo $row["name"] . " " . $row["description"] . "<br>" . "<br>";
-    };
-
-  }
-}
-
-$preparedquery->close();
-
-
-include("../dbase/dbclose.php");
-include("./footer.php");
-
-         ?>
-    </body>
-</html>
 ?>
