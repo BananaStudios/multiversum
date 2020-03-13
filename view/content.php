bf0042a6f1c4bd528aed165fe9db32f32f3c4502
Harisan02 <531493@edu.rocmn.nl>
2020-03-13 10:27:17 +0100

    --

--- view/content.php
+++ view/content.php
@@ -1,3 +1,4 @@
+
 <div class="container-fluid text-center mb-5 mt-3">
     <div class="card-deck justify-content-center">
       <?php require("overview.php"); ?>
--- view/footer.php
+++ view/footer.php
@@ -4,3 +4,5 @@
 <div class="footer">
     <p>&copy; Banana Studios <?php echo date ('Y'); ?><p>
 </div>
+
+</body>
