8bd6f33d3e4b8e0087c74e44f6de4577cf680b80
Harisan02 <531493@edu.rocmn.nl>
2020-03-13 11:31:50 +0100

    Update navbar.php

--- view/navbar.php
+++ view/navbar.php
@@ -17,7 +17,7 @@
     <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
       <ul class="nav navbar-nav navbar-right">
         <li>
-          <a href="#">Home</a>
+          <a href="./index.php">Home</a>
         </li>
         <li class="active">
           <a href="./contact.php">Contact</a>
