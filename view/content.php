b511f31e315fa382a5a13d62c68e77b0187d2610
iman-nab <iman_nab@hotmail.com>
2020-03-13 10:26:53 +0100

    hoi

--- view/content.php
+++ view/content.php
@@ -1,5 +1,5 @@
-<div class="container-fluid text-center mb-5 mt-3">
-    <div class="card-deck justify-content-center">
+<div class="container-fluid text-center row">
+    <div class="card-deck col-sm-6 ">
       <?php require("overview.php"); ?>
     </div>
 </div>
